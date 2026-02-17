0b22450f1d9675690c3df2d181adf38b0a00e9b5
Kishore114 <ka6311@srmist.edu.in>
2026-02-17 13:00:58 +0530

    Coded to UC2

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+// Hint use the Example beloe as a guide to construct each line of the banner
+// Example for OS which stands for Opeerating System:
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("    ***    " + "    ***     " + "******   "+"   ***** ");
+        System.out.println(" **     ** " + " **     **  " + "**    ** "+" **      ");
+        System.out.println("**       **" + "**       ** " + "**    ** "+"**       ");
+        System.out.println("**       **" + "**       ** " + "***** ** "+"  *****  ");
+        System.out.println("**       **" + "**       ** " + "**       "+"      ** ");
+        System.out.println(" **     ** " + " **     **  " + "**       "+"      ** ");
+        System.out.println("    ***    " + "    ***     " + "**       "+"  ***** "); 
+    }
+}
